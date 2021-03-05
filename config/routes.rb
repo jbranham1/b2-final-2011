3d296147f2a9a765dfd142668057df8b01d3594d
Jenny Branham <branhamjenny@gmail.com>
2021-03-05 11:24:34 -0600

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :flights
+  resources :flights, only: [:index, :show] do
+    resources :passengers, controller: "flight_passengers", only: [:destroy]
+  end
 end
