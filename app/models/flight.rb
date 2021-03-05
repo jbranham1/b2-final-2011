77156daca057da804d98d2abce84b7801ffbc9ff
Jenny Branham <branhamjenny@gmail.com>
2021-03-05 10:33:30 -0600

    Add order by departure city

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,8 @@ class Flight < ApplicationRecord
   has_many :passengers, through: :flight_passengers
 
   validates_presence_of :flight_number
+
+  def self.order_by_departure_city
+    order(departure_city: :desc)
+  end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
+  before :each do
+    @flight1 = Flight.create(flight_number: '1727',
+                            date: "08/03/20",
+                            time: "08/03/20",
+                            departure_city: "Denver",
+                            arrival_city: "Reno")
+    @flight2= Flight.create(flight_number: '1728',
+                            date: "08/03/20",
+                            time: "08/03/20",
+                            departure_city: "Little Rock",
+                            arrival_city: "Denver")
+  end
+
   describe 'relationships' do
     it { should have_many :flight_passengers }
     it { should have_many(:passengers).through(:flight_passengers) }
@@ -8,5 +21,13 @@ RSpec.describe Flight, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :flight_number }
+  end
+
+  describe 'instance methods' do
+    describe '::order_by_departure_city' do
+      it "orders flights by departure city alphabetically (a-z)" do
+        expect(Flight.order_by_departure_city.pluck(:departure_city)).to eq ([@flight2.departure_city, @flight1.departure_city])
+      end
+    end
   end
 end
