1f559bb4205350e1e034b09352e660dde1be04dc
Jenny Branham <branhamjenny@gmail.com>
2021-03-05 10:13:32 -0600

    Add relationships and validations to models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,6 @@
 class Flight < ApplicationRecord
+  has_many :flight_passengers
+  has_many :passengers, through: :flight_passengers
+
+  validates_presence_of :flight_number
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,7 @@
 class Passenger < ApplicationRecord
+  has_many :flight_passengers
+  has_many :flights, through: :flight_passengers
+
+  validates_presence_of :name
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
 end
