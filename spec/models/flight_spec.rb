5798a05ec4ad3d47e861651c2b196020876ef4ca
Jenny Branham <branhamjenny@gmail.com>
2021-03-05 10:11:00 -0600

    Add model specs

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
+  describe 'relationships' do
+    it { should have_many :flight_passengers }
+    it { should have_many(:passengers).through(:flight_passengers) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :flight_number }
+  end
 end
